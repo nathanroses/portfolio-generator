718708c07fa99400ce1b9cf61776bd7505cf1490
Nathan Rose <87315662+nathanroses@users.noreply.github.com>
2021-11-10 14:58:25 -0600

    file updated

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [Name, github] = profileDataArgs;
 const fs = require('fs');
 
+const profileDataArgs = process.argv.slice(2);
+
+const [Name, github] = profileDataArgs;
+
+const generatePage = require('./src/page-template');
 
 
-const generatePage = (Name, github) => {
-    return`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content"width=device-width, initial-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <title>Portfolio Demo</title>
-    </head>
-    
-    <body>
-      <h1>${Name}<h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `; 
-};
+
+
 
 fs.writeFile('index.html', generatePage(Name, github), err => {
     if (err) throw err;
