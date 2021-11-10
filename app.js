bc82db49dca065a9914567e2e6cdd65d0433648b
Nathan Rose <87315662+nathanroses@users.noreply.github.com>
2021-11-09 20:38:20 -0600

    generate page

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [Name, github] = profileDataArgs;
+const fs = require('fs');
 
 
 
-const generatePage = (userName, githubName) => {
+const generatePage = (Name, github) => {
     return`
-    Name: ${userName} 
-    Github: ${githubName}
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content"width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Portfolio Demo</title>
+    </head>
+    
+    <body>
+      <h1>${Name}<h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
     `; 
 };
 
-console.log(Name, github);
-console.log(generatePage(Name, github));
+fs.writeFile('index.html', generatePage(Name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio Complete! Check out index.html to see your output!')
+});
